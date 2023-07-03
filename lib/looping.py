fc82f1a8614009128b6abc19444f31e04d66474a
Shanley Caswell <shanleycaswell@gmail.com>
2023-07-03 17:09:56 -0400

    tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    number = 10
+    while number >= 1:
+        print(number)
+        number -= 1
+    print("Happy New Year!")
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int ** 2 for int in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    number = 1
+    while number <= 100:
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+        number += 1
